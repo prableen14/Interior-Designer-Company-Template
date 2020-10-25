e3339946917cf7fc09a72b2bc68c49e6bc2f26fc
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2020-10-25 15:51:32 +0530

    Add files via upload

---
+++ src/container/UI/Navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classes from './Navbar.module.css'
+
+const navbar = (props) => {
+  return (
+    <nav className={classes.Container}>
+      <span className={classes.Main}>Fake Navbar</span>
+      <div className={classes.Links}>
+        <span>Home</span>
+        <span>Link A</span>
+        <span>Link B</span>
+      </div>
+      {props.children}
+    </nav>
+  )
+}
+
+navbar.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object
+  ])
+}
+
+export default navbar
